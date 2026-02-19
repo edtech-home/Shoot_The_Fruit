de623f94058856b9b9bcbe69986a1b21bb6ea24f
Ed Tech <edtech.home@outlook.com>
2026-02-19 14:19:04 -0600

    Define on_mouse_down()

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ def draw():
     screen.clear()
     apple.draw()
 
+def place_apple():
+    apple.x = 300
+    apple.y = 200
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good shot!")
+        place_apple()
+        
+place_apple()
 pgzrun.go()
