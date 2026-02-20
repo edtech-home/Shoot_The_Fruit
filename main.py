9e7f41c3cd1cc32de7f6f973bee325b42b9a2602
Ed Tech <edtech.home@outlook.com>
2026-02-20 09:19:54 -0600

    Add score feature

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pgzrun
 import random
 
+score = 0
 apple = Actor("apple")
 def draw():
+    screen.draw.text(score, 50, 750)
     screen.clear()
     apple.draw()
 
@@ -10,11 +12,14 @@ def place_apple():
     apple.x = random.randint(10, 800)
     apple.y = random.randint(10, 600)
 def on_mouse_down(pos):
+    global score
     if apple.collidepoint(pos):
         print("Good shot!")
+        score += 1
         place_apple()
     else:
         print("You missed!")
+        score = 0
 
 place_apple()
 pgzrun.go()
